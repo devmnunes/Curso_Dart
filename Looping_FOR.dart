dc90c00d30c904be5ff54b9954c44fefe11097cc
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-04-02 16:45:42 -0300

    Update Looping_FOR.dart

--- Looping_FOR.dart
+++ Looping_FOR.dart
@@ -1,9 +1,24 @@
-void main (){
+void main() {
 
-for (int i = 100; i >= 0; i++) {
-  print(i);
-}
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      print('($i , $j)');
+    }
+  }
 
+  /*
+  //Contador de 1 a 10.
+  for (int i = 0; i <= 10; i++) {
+    print(i);
+  }
 
+  //Contador de 10 a 0.
+  for (int i = 10; i >= 0; i--) {
+    print(i);
+  }
 
+  //Contador de 0 a 10, pulando de 2 em 2.
+  for (int i = 0; i >= 10; i += 2) {
+    print(i);
+  } */
 }
