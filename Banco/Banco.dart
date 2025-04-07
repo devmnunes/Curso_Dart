ff224b6a59a77fcb56ddc15f0b66cdce42f1e28e
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-04-07 20:15:35 -0300

    commit

--- Banco/Banco.dart
+++ Banco/Banco.dart
@@ -1,3 +1,7 @@
+
+
+import 'Banco.dart';
+
 void main() {
   ContaBanco conta1 = ContaBanco('Matheus Nunes', 123, 'CC', 10.000, true);
   conta1.statusConta();
@@ -24,6 +28,32 @@ class ContaBanco {
       this.setSaldo = 50;
     } else if (tipoConta == 'CP') {
       this.setSaldo = 150;
+    }
+  }
+
+  void fecharConta() {
+    this.getStatus == true;
+    if (getSaldo > 0) {
+      print(
+          'Não foi possivel encerrar sua conta, saque o saldo restante da sua conta.');
+    } else if (getSaldo < 0) {
+      print(
+          'Não foi possivel encerrar sua conta, veja se há debitos em sua conta.');
+    } else {
+      print('Conta encerrada com sucesso.');
+      this.setStatus = false;
+    }
+  }
+
+  void sacar(double valor) {
+    if (getStatus == true) {
+      if (getSaldo > valor) {
+        setSaldo - valor;
+        
+      }
+      
+    }
+      
     }
   }
 
