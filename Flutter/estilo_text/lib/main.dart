3bae9583f09a828329fd626cdca7647b4d8aaa45
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-05-05 20:45:40 -0300

    commit

--- Flutter/estilo_text/lib/main.dart
+++ Flutter/estilo_text/lib/main.dart
@@ -10,13 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData( //Padrões pré definidas no app
+        fontFamily: 'Painterz' ,
+        brightness: Brightness.dark,  //App modo escuro
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          ),
+      ),
       home: Scaffold(
-        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Meu App',
+          ),
+        ),
+        
       body: Center(
         child: Text('Texto \n Texto 2',
         style: TextStyle(
           fontSize: 35,
-          fontFamily: 'Painterz',
           fontWeight: FontWeight.bold,
           color: Colors.black,
           backgroundColor: Colors.yellow,
