4deb8c71e542f4ca1ed85b417fb7e5b39e7ef15a
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-05-14 20:41:01 -0300

    commit

--- Flutter/requisicao_http/lib/main.dart
+++ Flutter/requisicao_http/lib/main.dart
@@ -1,3 +1,20 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main async {}
+void main() async {
+  Uri url = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+  final future = http.get(url);
+
+  future.then((response) {
+    if (response.statusCode == 200) {
+      print('Página carregada Ok');
+      //print(json.decode(response.body));
+      Map<String, dynamic> dados = json.decode(response.body);
+      print(dados);
+      print(dados['id']);
+    } else {
+      print('Erro');
+    }
+  });
+}
