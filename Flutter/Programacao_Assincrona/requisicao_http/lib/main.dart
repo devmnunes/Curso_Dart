518ae2709b0c35da6de17c384746e5bf512a90cb
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-05-16 12:07:11 -0300

    commit

--- Flutter/Programacao_Assincrona/requisicao_http/lib/main.dart
+++ Flutter/Programacao_Assincrona/requisicao_http/lib/main.dart
@@ -1,26 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
 
 void main() async {
-  Future future = getPosts();
+  
 
-  //Callbacks para tratar o valor e o erro
-  future.then((value) => print(value)).catchError((error) => print(error));
-}
 
-Future getPosts() async {
-  final client = http.Client(); //Cria uma instancia de client
-
-  try {
-    final uri = Uri.parse('https://jsonplaceholder.typicode.com/posts'); //REQUISITA API
-
-    final response = await client.get(uri); //AGUARDA A RESPOSTA DA API
-
-    final result = jsonDecode(response.body); //DECODIFICA O RESULTADO DA API
-
-    return result; // RETORNA O RESULTADO
-  } catch (e) {
-    print('error: $e');
-  }
-}
