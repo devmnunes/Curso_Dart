4ee688f32c590fa46fbef4ca7fbcf687511bdd74
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-05-16 13:02:30 -0300

    commit

--- Flutter/Programacao_Assincrona/requisicao_http/lib/main.dart
+++ Flutter/Programacao_Assincrona/requisicao_http/lib/main.dart
@@ -1,8 +1,15 @@
 import 'dart:async';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 
 void main() async {
-  
+  final future = http.get(Uri.https('www.example.com', '/'));
+  future.then((response) {
+    if (response.statusCode == 200) {
+      print('Página carregada');
+      } else {
+        print('Error');
+      }
+  });
 
-
+}
